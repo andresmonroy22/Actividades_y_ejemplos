572408ebcb8176911f2971a23d3d33c9675ac466
andresmonroy22 <145635288+andresmonroy22@users.noreply.github.com>
2023-11-01 11:42:24 -0500

    se agrega nueva actividad

---
+++ Talleres/Modulo 4/Taller_2_Sintaxis_Basica/app.js
@@ -0,0 +1,15 @@
+let variableslocales = "datos"
+{
+    var nombre="nombre"
+    var apellido="apellido"
+    console.log(nombre)
+    console.log(apellido)
+    let edad="edad"
+    let comida="comida"
+    console.log(edad)
+    console.log(comida)
+}
+    console.log(nombre)
+    console.log(apellido)
+    console.log(edad)
+    console.log(comida)
