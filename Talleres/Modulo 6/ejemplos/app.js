dff028a2a2dbf8292110db948276808fb234027b
andresmonroy22 <145635288+andresmonroy22@users.noreply.github.com>
2023-11-03 15:11:14 -0500

    Se realizan ejeḿplo 1

---
+++ Talleres/Modulo 6/ejemplos/app.js
@@ -0,0 +1,10 @@
+let nombre = "juan";
+let apellido = "hernesto";
+let edad = "18";
+let cancion_favorita = "amor eterno";
+
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(cancion_favorita)
+
