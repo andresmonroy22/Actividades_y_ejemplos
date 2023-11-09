63b015d3eec897e200089ab8d78a63e04d3cd1df
andresmonroy22 <145635288+andresmonroy22@users.noreply.github.com>
2023-11-08 22:06:29 -0500

    Se realiza taller 4

---
+++ Talleres/Modulo 6/talleres/Taller_4_Sintaxis_Bacica/app.js
@@ -0,0 +1,16 @@
+const N="Alex"
+const A="Marin"
+const E=30
+const CF="boogimian rapsody"
+
+console.log(N)
+console.log(A)
+console.log(E)
+console.log(CF)
+
+let booleano=true
+
+console.log(booleano)
+console.log("-------------------- taller 3")
+
+console.log("Hola! Mi nombre y apellido es ", N, A, "y tengo ", E, "años, mi canción favorita es ", CF, "y la respuesta es ", booleano)
