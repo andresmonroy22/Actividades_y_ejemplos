0dcdf48cb1e303dabebaad1d4744abe3a89b1a1f
andresmonroy22 <145635288+andresmonroy22@users.noreply.github.com>
2023-11-10 16:29:40 -0500

    Se realiza taller 5 modulo 6

---
+++ Talleres/Modulo 6/talleres/Taller_5_Sintaxis_Basica/app.js
@@ -0,0 +1,36 @@
+//punto 1
+//variables
+let A, B, C, Resultado
+
+A=10;
+B=15;
+C=2;
+
+//(a*b)/c
+Resultado=(A*B)/C
+console.log("El resultado es: ", Resultado)
+//(b+c)*a
+Resultado=(B+C)*A
+console.log("El resultado es: ", Resultado)
+//(a/b)-(a+c)
+Resultado=(A/B)-(A+C)
+console.log("El resultado es: ", Resultado)
+
+//punto 2
+//A+B-C%
+Resultado=A+B-C%
+console.log("El resultado es: ", Resultado)
+
+//punto 3
+//Incremento
+A++
+console.log(A)
+
+//punto 4
+ 
+//El operador Y (AND) &&
+//Verdadero => True
+//Falso => False
+resultado=true && true
+//Operador O (OR) ||
+resultado=false || false
