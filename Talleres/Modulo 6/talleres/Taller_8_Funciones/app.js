a6ebbb2b6f0fd15aa6a7773f5e2c77bcd8c6256c
andresmonroy22 <145635288+andresmonroy22@users.noreply.github.com>
2024-01-03 13:00:10 -0500

    se elimina el modulo 1

---
+++ Talleres/Modulo 6/talleres/Taller_8_Funciones/app.js
@@ -0,0 +1,26 @@
+//punto1
+
+function sumar(a,b){
+
+    return (a+b)
+}
+
+
+function resta(a,b){
+    
+    return (a-b)
+}
+
+function multiplicar(a,b){
+    
+    return (a*b)
+}
+
+function division(a,b){
+    
+    return (a/b)
+}
+
+let resultado=division(2,8)
+
+console.log(resultado)
